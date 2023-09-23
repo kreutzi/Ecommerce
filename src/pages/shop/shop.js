cec9de4c1bb3ef04b343fa0012238d3d1673a69e
yehia-codes <yahiamosa2004@gmail.com>
2023-09-24 02:45:30 +0300

    WIP

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./shop.css";
 
 function shop() {
-  return <div>shop</div>;
+  return (
+    <div clasName="shop">
+      <div className="header">
+        <div className="header-txt">
+          <div className="page">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>|</li>
+              <li className="active">About Us</li>
+            </ul>
+          </div>
+          <h1>About Our Store</h1>
+        </div>
+        <div className="headerImg"></div>
+      </div>
+    </div>
+  );
 }
 
 export default shop;
